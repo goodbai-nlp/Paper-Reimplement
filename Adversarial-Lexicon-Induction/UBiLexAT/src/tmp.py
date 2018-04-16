a1d0dcb7302184506249e707ac9a77fb28ae390b
muyeby <bxf_hit@163.com>
2018-04-16 19:13:15 +0800

    modified dir

--- Adversarial-Lexicon-Induction/UBiLexAT/src/tmp.py
+++
@@ -1,9 +0,0 @@
-import theano
-import theano.tensor as T
-import lasagne
-
-
-gen_input_var = T.matrix('gen_input_var')
-print "G",gen_input_var
-W=lasagne.init.Orthogonal()
-print "W",W
--- Adversarial-Lexicon-Induction/zhang2017acl/UBiLexAT/evaluate.sh
+++ Adversarial-Lexicon-Induction/zhang2017acl/UBiLexAT/evaluate.sh
@@ -2,8 +2,6 @@ source config
 
 reconWeight=1
 
-#THEANO_FLAGS="floatX=float32,device=gpu1" python src/linear_ae_ff_stable.py $config $lang1 $lang2 --num-minibatches 500000 --alt-loss --recon-weight --input-noise dropout --hidden-noise dropout $reconWeight > $reconWeight.log
-
 cut -d ' ' -f 1 data/$config/vocab-freq.$lang1 > data/$config/vocab.$lang1
 python3 scripts/translate.py data/$config/transformed-1.$lang1 data/$config/word2vec.$lang2 data/$config/vocab.$lang1 data/$config/result.1
 python3 evaluation/evaluate.py evaluation/ldc_cedict.txt data/$config/result.1
